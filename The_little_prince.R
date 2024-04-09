1c873f24cb8c45e49a1e0d17d44960a5aca26809
Alejandra Esquivel Arias <alejandraesqarias@gmail.com>
2024-04-09 17:28:51 -0400

    modifiying final chart

--- The_little_prince.R
+++ The_little_prince.R
@@ -265,83 +265,61 @@ plot_df <- tidy_lp %>%
   group_by(sentiment) %>%
   count(sentiment, sort = TRUE)
 
-
-
 # Basic radar chart
 plt <- ggplot(plot_df) +
   # Make custom panel grid
-  geom_hline(aes(yintercept = y), data.frame(y = c(0:1) * 100),color = "lightgrey") + 
-  # Add bars to represent the cumulative track lengths
-  # str_wrap(region, 5) wraps the text so each line has at most 5 characters
-  # (but it doesn't break long words!)
-  geom_col(
-    aes(x = reorder(str_wrap(sentiment, 5), n), y = n, fill = sentiment
-        ), position = "dodge2", show.legend = TRUE, alpha = .9) +
+  geom_col(aes(x = reorder(str_wrap(sentiment, 10), n), y = n, fill =sentiment
+  ), position = "dodge2", show.legend = TRUE, alpha = .9) +
   # Add dots to represent the mean gain
-  geom_point(
-    aes(x = reorder(str_wrap(sentiment, 5),n),y = n),
+  geom_point( aes(x = reorder(str_wrap(sentiment, 5),n),y = n),
     size = 3,color = "gray12") +
+  ylim(-150,max(plot_df$n)) +
   # Lollipop shaft for mean gain per region
-  geom_segment(aes(x = reorder(str_wrap(sentiment, 5), n),
-                   y = 0, xend = reorder(str_wrap(sentiment, 5), n), yend = 300),
+  geom_segment(aes(x = reorder(str_wrap(sentiment, 10), n),
+                   y = 0, xend = reorder(str_wrap(sentiment, 10), n), yend = 600),
                linetype = "dashed", color = "gray12") + 
   # Make it circular!
-  coord_polar() # + theme_bw()
-
-plt <- plt +
-  # Annotate the bars and the lollipops so the reader understands the scaling
-  annotate(x = 11, y = 600, label = "Mean Elevation Gain\n[FASL]",
-           geom = "text", angle = -67.5,color = "gray12", size = 2.5,
-           family = "Bell MT") +
-  # Scale y axis so bars don't start in the center
-  # scale_y_continuous(limits = c(-150, 350),
-  #                    expand = c(0, 0), breaks = c(0, 100, 200, 300)) + 
+  coord_polar(start = 0)  +
+  theme_minimal()
+ 
+  
+  
+plt <-  plt +
   # New fill and legend title for number of tracks per region
-  scale_fill_brewer(palette = "Purples") +
+  scale_fill_brewer(palette = "Purples") #+
   #   "Amount of Tracks",
-  #   colours = c( "#6C5B7B","#C06C84","#F67280","#F8B195") ) +
-  # Make the guide for the fill discrete
-  # guides( fill = guide_colorsteps(
-  #   barwidth = 15, barheight = .5, title.position = "top", title.hjust = .5)) +
   theme(
     # Remove axis ticks and text
     axis.title = element_blank(), axis.ticks = element_blank(),
     axis.text.y = element_blank(),
     # Use gray text for the region names
-    axis.text.x = element_text(color = "gray12", size = 12),
-    # Move the legend to the bottom
-    legend.position = "bottom",
-  )
+    axis.text.x = element_text(color = "gray12", size = 12)  )
 
 
 # Final chart
-# plt <- 
-plt + 
+plt <- plt +
   # Add labels
   labs(
-    title = "\n The Little prince feelins",
+    title = "\nEmotional Journey Through 'The Little Prince': \nA Sentiment Analysis Exploration",
     subtitle = paste(
-      "\nThis Visualisation shows the cummulative length of tracks,",
-      "the amount of tracks and the mean gain in elevation per location.\n",
-      "If you are an experienced hiker, you might want to go",
-      "to the North Cascades since there are a lot of tracks,",
-      "higher elevations and total length to overcome.",
+      "This visualization depicts the predominant emotional landscape of 'The Little Prince',",
+      "highlighting the frequency of various sentiments throughout the narrative.\n",
+      "If you're exploring the emotional depth of literature, 'The Little Prince' offers a rich terrain,",
+      "with diverse sentiments ranging from joy and trust to sadness and anger.",
       sep = "\n"
     ),
-    caption = "\n\nData Visualisation by Tobias Stalder\ntobias-stalder.netlify.app\nSource: TidyX Crew (Ellis Hughes, Patrick Ward)\nLink to Data: github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-11-24/readme.md") +
+    caption = "\n\nData Visualisation by Alejandra Esquivel\nLink to Data: github.com/aesquivel94/The_Little_Prince") +
   # Customize general theme
   theme(
     # Set default color and font family for the text
     text = element_text(color = "gray12", family = "Bell MT"),
-    # Customize the text in the title, subtitle, and caption
-    plot.title = element_text(face = "bold", size = 25, hjust = 0.05),
-    plot.subtitle = element_text(size = 14, hjust = 0.05),
-    plot.caption = element_text(size = 10, hjust = .5),
     # Make the background white and remove extra grid lines
-    panel.background = element_rect(fill = "white", color = "white"),
-    panel.grid = element_blank(),
-    panel.grid.major.x = element_blank()
-  )
+    panel.background = element_rect(fill = "white", color = "white")
+  ) 
+
 # Use `ggsave("plot.png", plt,width=9, height=12.6)` to save it as in the output
 
+plt +
+  scale_x_discrete(labels = paste0(toupper(substr(plot_df$sentiment, 1, 1)), substr(plot_df$sentiment, 2, nchar(plot_df$sentiment))) )
 
+                   
